22a175016a889d27561512a06bb51df55568cd39
Emily Switzer <emily-switzer@live.com>
2021-11-24 17:04:26 +0000

    Factory method complete. 2 factories for creating obstacles in different ways

--- src/BoxGame.java
+++ src/BoxGame.java
@@ -3,11 +3,8 @@ public class BoxGame {
     public static void main(String[] args) {
 
         ObstacleFactory obstacleFactory = new NPCFactory();
-        Obstacle fence = obstacleFactory.generateObstacle(FlyingObstacleType.BIRD);
-        Obstacle spike = obstacleFactory.generateObstacle(FlyingObstacleType.CLOUD);
-
-        System.out.println(fence);
-        System.out.println(spike);
+        System.out.println(obstacleFactory.generateObstacle(FlyingObstacleType.BIRD));
+        System.out.println(obstacleFactory.generateObstacle(FlyingObstacleType.CLOUD));
 
         ObstacleFactory randomFactory = new NPCRandomFactory();
         System.out.println((randomFactory.generateRandomObstacle()));
